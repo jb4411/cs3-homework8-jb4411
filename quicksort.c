e736532e4d878234c337f461f88a6f5131bb545d
Jesse  Burdick-Pless <jb4411@cs.rit.edu>
2020-11-15 15:38:55 -0500

    Began work on threaded quicksort implementation.

--- quicksort.c
+++ quicksort.c
@@ -8,6 +8,13 @@
 #include <getopt.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+/// num_spawned is a variable manipulated by multiple threads.
+static int num_spawned = 0;
+
+/// sharedLock protects the variable from concurrent access.
+static pthread_mutex_t sharedLock;
 
 int **partition( int pivot, size_t size, const int *data ) {
 	int less_size = 0, same_size = 0, more_size = 0;
@@ -96,7 +103,55 @@ int *quicksort( size_t size, const int *data ) {
 }
 
 void *quicksort_threaded( void *args ) {
+	// args layout:  args[0] = size, args[1] = *data
+	if( size == 0 ) {
+                return NULL;
+        } else {
+		size_t size = (size_t) args[0];
+		int *data = args[1]; //TODO
+                int pivot = (int) data[0]; // TODO
+		int **parts = partition(pivot, size, data);
+                int less_size = parts[3][0];
+                int same_size = parts[3][1];
+                int more_size = parts[3][2];
+                // sort less
+                int *old_less = parts[0];
+                int *less = quicksort(less_size, old_less);
+                if( old_less != NULL ) {
+                        free(old_less);
+                }
 
+                int *same = parts[1];
+
+                // sort more
+                int *old_more = parts[2];
+                int *more = quicksort(more_size, old_more);
+                if( old_more != NULL ) {
+                        free(old_more);
+                }
+
+                // combine less + same + more into result
+                int *result = calloc(size, sizeof(int));
+                assert(result != NULL);
+                if( less != NULL ) {
+                        memcpy(result, less, sizeof(int) * less_size);
+                }
+                if( same != NULL ) {
+                        memcpy(result + less_size, same, sizeof(int) * same_size);
+                }
+                if( more != NULL ) {
+                        memcpy(result + less_size + same_size, more, sizeof(int) * more_size);
+                }
+
+                // free memory that is no longer needed
+                free(less);
+                free(same);
+                free(more);
+                free(parts[3]);
+                free(parts);
+
+                return result;
+        }
 }
 
 int main( int argc, char **argv ) {
