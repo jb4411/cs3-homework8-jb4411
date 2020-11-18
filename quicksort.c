fd178e49313c95594960b07522b4438f7c2f9f33
Jesse  Burdick-Pless <jb4411@cs.rit.edu>
2020-11-18 13:19:26 -0500

    Fixed error staements and fixed a memory leak.

--- quicksort.c
+++ quicksort.c
@@ -166,7 +166,7 @@ int *quicksort( size_t size, const int *data ) {
 void *quicksort_threaded( void *args ) {
 	int *data = (int *) args;
 	if( data[0] == 0) {
-                pthread_exit(NULL);
+                return NULL;
         } else {
 		pthread_t threads[2];
 		int rc = 0;
@@ -186,9 +186,10 @@ void *quicksort_threaded( void *args ) {
 		memcpy(less_args + 1, old_less, sizeof(int) * less_size);
 		rc = pthread_create( &threads[0], NULL, quicksort_threaded, (void *) less_args );
 		if( rc ) {
-			printf( "ERROR; pthread_create() returned %d\n", rc );
-			exit( EXIT_FAILURE );
-		}
+                        errno = rc;
+                        perror( "pthread_create" );
+                        exit( EXIT_FAILURE );
+                }
 
 		int *same = parts[1];
 
@@ -198,33 +199,47 @@ void *quicksort_threaded( void *args ) {
                 more_args[0] = more_size;
                 memcpy(more_args + 1, old_more, sizeof(int) * more_size);
                 rc = pthread_create( &threads[1], NULL, quicksort_threaded, (void *) more_args );
-                if( rc ) {
-                        printf( "ERROR; pthread_create() returned %d\n", rc );
+		if( rc ) {
+                        errno = rc;
+                        perror( "pthread_create" );
                         exit( EXIT_FAILURE );
                 }
 
 		// join less and more threads
 		void *retval = NULL;
-		pthread_join(threads[0], &retval); 
+		rc = pthread_join(threads[0], &retval); 
+		if( rc ) {
+			errno = rc;
+			perror( "pthread_join" );
+			exit( EXIT_FAILURE );
+		}
 		int *less_rt = (int *) retval;
 		int *less = &less_rt[1];
-		pthread_join(threads[1], &retval);
+		
+		rc = pthread_join(threads[1], &retval);
+		if( rc ) {
+                        errno = rc;
+                        perror( "pthread_join" );
+                        exit( EXIT_FAILURE );
+                }
 		int *more_rt = (int *) retval;
 		int *more = &more_rt[1];
 
 		// update num_spawned
-                int lock = pthread_mutex_lock(&sharedLock);
-                if( lock ) {
-                        perror( "locking to update num_spawned" );
-                        exit( errno );
+		int lock = pthread_mutex_lock(&sharedLock);
+		if( lock ) {
+                        errno = lock;
+                        perror( "pthread_mutex_lock" );
+                        exit( EXIT_FAILURE );
                 }
-                // +1 for the thread for less, +1 for the thread for more
-                num_spawned += 2;
-                lock = pthread_mutex_unlock(&sharedLock);
-                if( lock ) {
-                        perror( "unlocking after updating num_spawned" );
-                        exit( errno );
-                }
+		// +1 for the thread for less, +1 for the thread for more
+		num_spawned += 2;
+		lock = pthread_mutex_unlock(&sharedLock);
+		if( lock ) {
+			errno = lock;
+			perror( "pthread_mutex_unlock" );
+			exit( EXIT_FAILURE );
+		}
 
 		// free old_less and old_more
 		if( old_less != NULL ) {
@@ -262,7 +277,7 @@ void *quicksort_threaded( void *args ) {
 		free(parts[3]);
 		free(parts);
 
-		pthread_exit((void *) result);
+		return (void *) result;
 	}
 }
 
@@ -376,7 +391,8 @@ int main( int argc, char **argv ) {
 	// sort (threaded) and print the timing
 	int rc = pthread_mutex_init( &sharedLock, NULL );
 	if( rc ) {
-		printf( "ERROR; pthread_mutex_init() returned %d\n", rc );
+		errno = rc;
+		perror( "pthread_mutex_init" );
 		exit( EXIT_FAILURE );
 	}
 	pthread_t thread;
@@ -388,15 +404,27 @@ int main( int argc, char **argv ) {
 	start = clock();
 	rc = pthread_create( &thread, NULL, quicksort_threaded, (void *) args );
 	if( rc ) {
-		printf( "ERROR; pthread_create() returned %d\n", rc );
+		errno = rc;
+		perror( "pthread_create" );
 		exit( EXIT_FAILURE );
 	}
-	pthread_join(thread, &retval);	
+	rc = pthread_join(thread, &retval);	
+	if( rc ) {
+		errno = rc;
+		perror( "pthread_join" );
+		exit( EXIT_FAILURE );
+	}
 	end = clock();
 	total_time = (double) (end - start) / CLOCKS_PER_SEC;
 	printf("Threaded time: %f\n", total_time);
 	printf("Threads spawned: %d\n", num_spawned);
-	pthread_mutex_destroy(&sharedLock);
+	rc = pthread_mutex_destroy(&sharedLock);
+	if( rc ) {
+		errno = rc;
+		perror( "pthread_mutex_destroy" );
+		exit( EXIT_FAILURE );
+	}
+
 
 	// print the sorted values if -p
 	if( print ) {
